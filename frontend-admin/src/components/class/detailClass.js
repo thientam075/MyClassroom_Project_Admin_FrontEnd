bb5da870c17a354781caee27a30c80250d6515cc
HuyThien <57531802+HuyThien@users.noreply.github.com>
2022-01-18 12:32:35 +0700

    edit detail Class

---
+++ frontend-admin/src/components/class/detailClass.js
@@ -0,0 +1,50 @@
+
+import {useState} from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+
+import Moment from 'react-moment';
+import 'moment-timezone';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+export default function DetailClass({fullname, subject, createdAt}) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Chi tiết</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <h2 id="parent-modal-title">Tên lớp: {fullname}</h2>
+          <h2 id="parent-modal-title">Môn học: {subject}</h2>
+          <h2 id="parent-modal-title">Ngày tạo: {createdAt}</h2>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
